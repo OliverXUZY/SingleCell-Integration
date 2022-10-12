31bdecd87895941c6e1d8e27bdf584b51b72c0ec
Zhuoyan Xu <zxu68341@icloud.com>
2022-10-12 15:24:24 -0500

    dataset minibatch

--- func/prepro.py
+++ func/prepro.py
@@ -1,5 +1,13 @@
 import h5py
 import hdf5plugin
+
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 def readH5pyFile(filename):
     with h5py.File(filename, "r") as f:
         # Print all root level object names (aka keys) 
@@ -25,3 +33,40 @@ def readH5pyFile(filename):
             d[i] = group[i][()]
         
     return d
+
+
+class SingleCellDataset(Dataset):
+    def __init__(self, path_to_input_file, path_to_target_file) -> None:
+        self.input_file = path_to_input_file
+        self.target_file = path_to_target_file
+        with h5py.File(self.input_file, "r") as f:
+            a_group_key = list(f.keys())[0]            
+            group = f[a_group_key]      # returns as a h5py dataset object
+            
+            self.cells = group['axis1'][:].astype(str)
+            self.features = group['axis0'][:].astype(str)
+        
+        with h5py.File(self.target_file, "r") as f:
+            a_group_key = list(f.keys())[0]            
+            group = f[a_group_key]      # returns as a h5py dataset object
+            
+            self.targets = group['axis0'][:].astype(str)
+
+    def __len__(self):
+        return len(self.cells)
+
+    def __getitem__(self, index):
+        with h5py.File(self.input_file, "r") as f:
+            a_group_key = list(f.keys())[0]
+            group = f[a_group_key]      # returns as a h5py dataset object
+            
+            cells, inputs = self.cells[index], group['block0_values'][index]
+        
+        with h5py.File(self.target_file, "r") as f:
+            a_group_key = list(f.keys())[0]            
+            group = f[a_group_key]      # returns as a h5py dataset object
+            
+            targets = group['block0_values'][index]
+        
+        return cells, inputs, targets
+            
