6048f71168df6e752c4bf08a199d5df657b401e8
zxu444 <zxu444@submit-1.chtc.wisc.edu>
2022-10-21 15:32:47 -0500

    testStaing

---
+++ CHTC/shuchen/testStaging.py
@@ -0,0 +1,19 @@
+import sys
+import pandas as pd
+import prepro as pp
+print('sys.argv is', sys.argv)
+
+file1 = sys.argv[1] ## test_cite_inputs
+file2 = sys.argv[2] ## test_multi_inputs
+
+multi_input = pp.readH5pyFile(file1) ## file1 is train_multi_inputs
+multi_target = pp.readH5pyFile(file2) ## file2 is train_multi_targets
+res1 = 0  ## your first prediction matrix based on test_cite_inputs
+
+res2 = 0  ## your second prediction matrix based on test_multi_inputs
+
+# res1.to_csv("preMat_protein.csv.gz",compression='gzip')
+# res2.to_csv("preMat_RNA.csv.gz",compression='gzip')
+
+print(len(multi_input))
+print(len(multi_target))
