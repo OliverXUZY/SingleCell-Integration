ceee8368d05549a2c1e4053c16cac6b4527fea4f
Shuchen Yan <ysc12451@163.com>
2022-10-22 18:30:37 -0500

    test 1021

--- CHTC/shuchen/runPy.sh
+++ CHTC/shuchen/runPy.sh
@@ -2,7 +2,7 @@
 
 # First, copy the compressed tar file from /staging into the working directory,
 #  and un-tar it to reveal your large input file(s) or directories:
-cp /staging/username/large_input.tar.gz ./
-cp /staging/username/large_input.tar.gz ./
+cp /staging/syan58/train_multi_inputs.h5 ./
+cp /staging/syan58/train_multi_targets.h5 ./
 
-python testStaging.py $1 $2
+python testStaging.py $1
