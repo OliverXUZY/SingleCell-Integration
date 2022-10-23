64f5cfab45040c3796259a5b569298e79d995901
Shuchen Yan <ysc12451@163.com>
2022-10-22 23:04:24 -0500

    Update testStaging.py

--- CHTC/shuchen/testStaging.py
+++ CHTC/shuchen/testStaging.py
@@ -1,13 +1,15 @@
 import sys
+import os
 import pandas as pd
 import prepro as pp
 print('sys.argv is', sys.argv)
+print("current working directory is", os.getcwd())
 
-file1 = sys.argv[1] ## test_cite_inputs
-file2 = sys.argv[2] ## test_multi_inputs
+# file1 = sys.argv[1] ## test_cite_inputs
+# file2 = sys.argv[2] ## test_multi_inputs
 
-multi_input = pp.readH5pyFile(file1) ## file1 is train_multi_inputs
-multi_target = pp.readH5pyFile(file2) ## file2 is train_multi_targets
+multi_input = pp.readH5pyFile("train_multi_inputs.h5") ## file1 is train_multi_inputs
+multi_target = pp.readH5pyFile("train_multi_targets.h5") ## file2 is train_multi_targets
 res1 = 0  ## your first prediction matrix based on test_cite_inputs
 
 res2 = 0  ## your second prediction matrix based on test_multi_inputs
