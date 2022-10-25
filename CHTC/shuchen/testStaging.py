c021459dc80da52934614427c74aed26ab4ca52c
ysc12451 <ysc12451@163.com>
2022-10-25 11:20:08 -0500

    debug 1025

--- CHTC/shuchen/testStaging.py
+++ CHTC/shuchen/testStaging.py
@@ -8,14 +8,13 @@ print("current working directory is", os.getcwd())
 # file1 = sys.argv[1] ## test_cite_inputs
 # file2 = sys.argv[2] ## test_multi_inputs
 
-multi_input = pp.readH5pyFile("train_multi_inputs.h5") ## file1 is train_multi_inputs
-multi_target = pp.readH5pyFile("train_multi_targets.h5") ## file2 is train_multi_targets
+multi_input = pp.readH5pyFile("/staging/syan58/open-problems-multimodal/raw/train_multi_inputs.h5") ## file1 is train_multi_inputs
+# multi_target = pp.readH5pyFile("/staging/syan58/open-problems-multimodal/raw/train_multi_targets.h5") ## file2 is train_multi_targets
 res1 = 0  ## your first prediction matrix based on test_cite_inputs
-
 res2 = 0  ## your second prediction matrix based on test_multi_inputs
 
 # res1.to_csv("preMat_protein.csv.gz",compression='gzip')
 # res2.to_csv("preMat_RNA.csv.gz",compression='gzip')
 
 print(len(multi_input))
-print(len(multi_target))
+# print(len(multi_target))
