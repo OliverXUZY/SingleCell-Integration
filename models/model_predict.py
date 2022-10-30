6cdfb1f446e45b75028d9fb13c239d754ab4624a
Zhuoyan Xu <zxu68341@icloud.com>
2022-10-30 16:59:07 -0500

    add Linear regression model

--- models/model_predict.py
+++ models/model_predict.py
@@ -7,5 +7,21 @@ from func.prepro import SingleCellDataset, SingleCellDataset_test
 def test10(inputs):
     return torch.matmul(inputs, torch.ones(228942, 10))  # for SingleCellDataset_test
 
+def test20(inputs):
+    return torch.matmul(inputs, torch.ones(228942, 20))  # for SingleCellDataset_test
+
 def testfull(inputs):
     return torch.matmul(inputs, torch.ones(228942, 23418)) # SingleCellDataset
+
+
+class LinearRegression(nn.Module):
+    def __init__(self, weightPCA) -> None:
+        super(LinearRegression, self).__init__()
+
+        self.LR = nn.Linear(1000,100, bias=True)
+        self.weightPCA = weightPCA
+
+    def forward(self, input):
+        input = input.matmul(self.weightPCA)
+        output = self.LR(input)
+        return output
