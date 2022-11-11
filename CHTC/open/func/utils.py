94dd5ffdfbcc7c18ae9f40d26be0fd2a8e34fb75
zxu444 <zxu444@submit-1.chtc.wisc.edu>
2022-11-11 13:44:59 -0600

    utils.py

--- CHTC/open/func/utils.py
+++ CHTC/open/func/utils.py
@@ -10,3 +10,10 @@ def log(obj, filename='log.txt'):
     if _log_path is not None:
         with open(os.path.join(_log_path, filename), 'a') as f:
             print(obj, file=f)
+
+def time_str(t):
+  if t >= 3600:
+    return '{:.1f}h, {:.1f}min'.format(t / 3600, (t % 3600)/60) # time in hours
+  if t >= 60:
+    return '{:.1f}m, {:.1f}s'.format(t / 60, t % 60)   # time in minutes
+  return '{:.1f}s'.format(t)
